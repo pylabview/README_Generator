dda376f164011a5d214f72ec9a604c73f0defe79
pylabview <rodrigo19662001@gmail.com>
2023-05-19 11:06:35 -0700

    Adding functionality, need to test

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const inquirer = require("inquirer");
 const fs = require("fs");
 const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/
-const fetch = (...args) => import('node-fetch').then(({default: fetch}) => fetch(...args));
+
 // Helper Functions
 // Input validation 
 const checkTextLength = (str,min_len,msg) => {
@@ -17,7 +17,7 @@ const checkTextLength = (str,min_len,msg) => {
     if(arrayWords.length >= min_len){
         return true;
     }else {
-        console.log(msg);
+        console.log('\n' + msg);
         return false;
     }
 };
@@ -26,42 +26,70 @@ const checkUserRepo = (repoName,msg) => {
     if(repoName){
         return true;
     }else{
-        console.log(msg);
+        console.log("\n" + msg);
         return false;
     }
     };
-
-
 
 const checkParagraphLines = (txt,min_lines,msg) => {
     let lines = txt.split("\n").length - 1;
     if(lines >= min_lines){
         return true;
     }else {
-        console.log(msg);
+        console.log("\n" + msg);
         return false;
     }
 };
 
-const checkEMail = (email) => {
+const checkEMail = (email, msg) => {
     if(emailRegex.test(email)){
         return true;
     } else {
-        console.log(" Email address is not valid");
+        console.log("\n" + msg);
         return false;
     }
 }
 
-// 
 
-// 
+
+const generateREADME = ({github_username,
+                        email,
+                        title,
+                        description,
+                        installation_instructions,
+                        license_type,
+                        usage_instructions,}) =>
+                        `# ${title}
+
+                        ## Description
+                        ${description}
+                        
+                        ## Table of Content
+                        - [Installation](#installation)
+                        - [Usage](#usage)
+                        - [Credits](#credits)
+                        - [License](#license)
+                        
+                        ## Installation
+                        ${installation_instructions}
+                        ## Usage
+                        ${usage_instructions}
+                        ## Credits
+                        
+                        ## Licence
+                        ![](https://img.shields.io/badge/license-${license_type}-blue) 
+                        
+                        ## Contact
+                        User: ${github_username}
+                        Email: ${email}
+                        `;
 
 
 inquirer.prompt([
     {
         type: 'input',
         name: 'github_username',
-        message: "Enter enter my GitHub username",
+        message: "Enter enter my GitHub username: ",
         default: () => {},
         validate: (github_username) => checkUserRepo(github_username,
             "Please enter a valid GitHub username")
@@ -69,14 +97,14 @@ inquirer.prompt([
     {
         type: 'input',
         name: 'email',
-        message: "Enter your email address",
+        message: "Enter your email address: ",
         default: () => {},
-        validate: (email) => checkEMail(email),
+        validate: (email) => checkEMail(email, 'Email address is not valid'),
     },
     {
         type: 'input',
         name: 'title',
-        message: 'What is your the project Title',
+        message: 'What is your the project Title:',
         default: () => {},
         validate: (title) => checkTextLength(title,1,"The Title must be at least one word")
     }
@@ -84,14 +112,14 @@ inquirer.prompt([
     {
         type: 'input',
         name: 'description',
-        message:'Enter Project description',
+        message:'Enter Project description: ',
         default: () => {},
         validate: (description) => checkTextLength(description,3,"The Description must be at least three word")
     },
     {
         type: 'editor',
         name: 'installation_instructions',
-        message: 'Enter usage information',
+        message: 'Enter installation instructions: ',
         default: () => {},
         validate: (installation_instructions) => 
                     checkParagraphLines(installation_instructions,1,"Must be at least one line")
@@ -101,17 +129,27 @@ inquirer.prompt([
         message: 'Enter license type',
         name: 'license_type',
         default: 'ISC',
-        choices: ['Apache license 2.0', 
-                    'GNU Lesser General Public License v3.0', 
-                    'ISC',
+        choices: ['ISC',
+                  'Apache_v2.0', 
+                    'GNU_v3.0', 
                     'MIT',
-                    'Boost Software License 1.0'],
+                    'BSL_1.0',],
       },
+    {
+        type: 'editor',
+        name: 'usage',
+        message: 'Enter usage information: ',
+        default: () => {},
+        validate: (installation_instructions) => 
+                    checkParagraphLines(installation_instructions,1,"Must be at least one line")
+    },
 
 ]).then((answer) => {
     console.log(answer);
-
-
+    const readme = generateREADME(answer);
+    fs.writeFile('README.md', readme, (err) =>
+    err ? console.log(err) : console.log('Successfully created README.md!')
+  );
 
 } 
 );
